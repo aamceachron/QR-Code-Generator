f7cf5272abcaf163b01213f03eeb3b019840bdf1
aamceachron <56684775+aamceachron@users.noreply.github.com>
2023-06-06 15:43:15 +0000

    I decided to rewrite the code as the structure was getting out of hand. Keeping it simple with the core functions of what is needed and then will be gradually building the function out.

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import qrcode
 
-print("QR Code Generator \n")
+print("General Purpose QR Code Generator \n")
 
-web_link = input("Please type or paste your web link here: ")
-web_link = web_link.replace(" ", "%20")
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(web_link)
-qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("qrcode.png")
-print("\nQR Code Generated Successfully!")
+web_link = qrcode.make(input("Please type or paste your web link here: "))
+web_link.save("qrcode.png")
+
+print("\n")  #general spacing for visual consisancy
+
+print("QR code generated successfully!")
