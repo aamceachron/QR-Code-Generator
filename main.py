999979be4e7009a0e2ef3cfa90406166b9ab6479
aamceachron <56684775+aamceachron@users.noreply.github.com>
2023-06-06 15:13:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import qrcode
+
+print("Kings Data & Design - QR Code Generator \n")
+
+web_link = input("Enter the web link: ")
+web_link = web_link.replace(" ", "%20")
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(web_link)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qrcode.png")
+print("\nQR Code Generated Successfully!")
